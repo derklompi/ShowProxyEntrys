308f83d6a475e94b7541743225b5d2a95dda3a53
Fabian Dörr <faflfama@outlook.com>
2017-12-22 14:51:07 +0100

    Projektdateien hinzufügen.

---
+++ ShowProxyEntrys/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowProxyEntrys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+
+            string name;
+
+            name = Convert.ToString(key.GetValue("ProxyOverride"));
+
+            string[] names = name.Split(';');
+
+            foreach (string a in names)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("");
+            Console.Write("Bitte drücken Sie eine Taste um das Programm zu beenden...");
+            Console.ReadKey();
+        }
+    }
+}
